30b00f029e6793398133e722634347eb9fb06718
FavianaT <favianat802@gmail.com>
2025-12-08 17:50:40 -0500

    _

--- src/main/java/entity/product.java
+++ src/main/java/entity/product.java
@@ -5,58 +5,61 @@ public class Product {
     private String name;
     private String description;
     private String color;
-    private decimal size;
-    private decimal price;
+    private double size;
+    private double price;
+
+    public Product(int ID, String name, String description, String color, double size, double price) {
+        this.ID = ID;
+        this.name = name;
+        this.description = description;
+        this.color = color;
+        this.size = size;
+        this.price = price;
+    }
+    
+    public Product(int ID, String name, String color, double size, double price) {
+        this.ID = ID;
+        this.name = name;
+        this.color = color;
+        this.size = size;
+        this.price = price;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "ID=" + ID + ", name=" + name + ", description=" + description + ", color=" + color + ", size=" + size + ", price=" + price + '}';
+    }   
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
-
-public class Product {
-    private int ID;
-    private String name;
-    private String color;
-    private decimal size;
-    private decimal price;
-}
-
-public class Product {
-
-}
-
-public int getID() {
-    return ID;
-}
-
-public String getName() {
-    return name;
-}
-
-public String getDescription() {
-    return description;
-}
-
-public String getColor() {
-    return color;
-}
-
-public decimal getSize() {
-    return size;
-}
-
-public decimal getPrice() {
-    return price;
-}
-
-@Override
-public String toString() {
-    return "Product{" + "ID=" + ID + ", name=" + name + ", description=" + description + ', color=' + color + ', size=' + size + ', price=' + price + '}';
-}
-
-public void setName(String name) {
-    this.name = name;
-}
-
-public void setDescription(String description) {
-    this.description = description;
-}
-  
-
-  
