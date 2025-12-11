9b1e9f83ebb9340a343961eb757261b1c8557df4
FavianaT <favianat802@gmail.com>
2025-12-11 13:35:43 -0500

    _

--- src/main/resources/db/migrations/V2__Create_SS_Product.sql
+++ src/main/resources/db/migrations/V2__Create_SS_Product.sql
@@ -2,7 +2,7 @@ CREATE TABLE SS_Product (
     Product_ID int NOT NULL PRIMARY KEY,
     Product_Name VARCHAR(20) NOT NULL,
     Product_Description VARCHAR(50) NOT NULL,
-    Product_Color VARCHAR(50) NOT NULL,
+    Product_Color VARCHAR(20) NOT NULL,
     Product_Size decimal(10,2) NOT NULL,
     Product_Price decimal(10,2) NOT NULL
 );
