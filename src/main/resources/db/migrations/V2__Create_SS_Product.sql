0d14f072a21c7efa3a2ed52d461a99985eea0d03
FavianaT <203860486+FavianaT@users.noreply.github.com>
2025-12-16 22:23:16 -0500

    _

--- src/main/resources/db/migrations/V2__Create_SS_Product.sql
+++ src/main/resources/db/migrations/V2__Create_SS_Product.sql
@@ -1,8 +1,16 @@
 CREATE TABLE SS_Product (
-    Product_ID int NOT NULL PRIMARY KEY,
+    Product_ID int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     Product_Name VARCHAR(20) NOT NULL,
     Product_Description VARCHAR(50) NOT NULL,
     Product_Color VARCHAR(20) NOT NULL,
     Product_Size decimal(10,2) NOT NULL,
     Product_Price decimal(10,2) NOT NULL
 );
+
+INSERT INTO SS_Product (Product_Name, Product_Description, Product_Color, Product_Size, Product_Price)
+VALUES ('Nike Zoom', 'Running Shoes made for wet weather protection',
+        'Black', 8.00, 80.00);
+
+INSERT INTO SS_Product (Product_Name, Product_Description, Product_Color, Product_Size, Product_Price)
+VALUES ('Puma Sneakers', 'Breathable for everyday use', 'White',
+        6.5, 70.00)
